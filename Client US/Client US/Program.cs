124bbca2e0fd1be13b8ea4d65bca3a78ebfa0cb1
Daniil Pogovorov <48155348+PogovorovDaniil@users.noreply.github.com>
2021-02-18 17:59:08 +0300

    /

--- Client US/Client US/Program.cs
+++ Client US/Client US/Program.cs
@@ -7,25 +7,39 @@ namespace Client_US
 {
     class Program
     {
+        static Socket socket;
+        static EndPoint endPoint;
+
         static void Main()
         {
-            //FileAndHash[] files = HashList.GetFileList(Directory.GetCurrentDirectory() + "\\dir\\");
-            //foreach (FileAndHash file in files) Console.WriteLine(file);
-
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            EndPoint endPoint = new IPEndPoint(IPAddress.Parse("193.178.169.223"), 9090);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            endPoint = new IPEndPoint(IPAddress.Parse("193.178.169.223"), 9090);
 
             socket.Connect(endPoint);
-            while (true)
+            Console.WriteLine("Connected");
+            try
             {
-                int a = int.Parse(Console.ReadLine());
-                int b = int.Parse(Console.ReadLine());
-                socket.Send(new byte[] {(byte)a, (byte)b});
-                byte[] buffer = new byte[1];
-                socket.Receive(buffer);
+                while (true)
+                {
+                    //FileAndHash[] files = HashList.GetFileList(Directory.GetCurrentDirectory() + "\\dir\\");
+                    //foreach (FileAndHash file in files) Console.WriteLine(file);
 
-                Console.WriteLine(buffer[0]);
+                    int act = int.Parse(Console.ReadLine());
+                    socket.Send(new byte[] {(byte)act});
+                    byte[] buffer = new byte[1];
+                    int bytesCount = socket.Receive(buffer);
+                    if (bytesCount == 0) break;
+
+                    Console.WriteLine(buffer[0]);
+                }
             }
+            catch(Exception)
+            {
+                socket.Close();
+                Console.WriteLine("Lost connection");
+            }
+            Console.WriteLine("End receive");
+            while (true) ;
         }
     }
 }
--- Server US/serverUS.py
+++ Server US/serverUS.py
@@ -5,23 +5,39 @@ sock = socket.socket()
 sock.bind(('', 9090))
 sock.listen(256)
 
+
+def SendFileList(conn):
+	print('SendFileList run')
+	conn.send(bytes([1]))
+	return 0
+
+def SendVersion(conn):
+	conn.send(bytes([2]))
+	return 0
+
+serverActions = [SendFileList, SendVersion]
+
 def AcceptClients(conn, addr):
 	try:
 		print('connected:', addr)
 		while True:
-			data = conn.recv(2)
+			data = conn.recv(1)
 			if not data:
 				break
-			a = int(data[0])
-			b = int(data[1])
-			print(str(a) + ' + ' + str(b) + ' = ' + str(a + b))
-			conn.send(bytes([a + b]))
-	except:
-		print('disconnected:', addr)
+			actId = int(data[0])
+			
+			if actId >= len(serverActions):
+				raise ValueError('Bad actId')
+			if serverActions[actId](conn):
+				raise ValueError('Action error')
+
+	except Exception as ex:
+		print('disconnected:', addr, ' By reason: ', ex)
 		conn.close()
 
+def AcceptListener():
+	while True:
+		Thread(target=AcceptClients, args=sock.accept()).start()
 
 if __name__ == '__main__':
-	while True:
-		th = Thread(target=AcceptClients, args=sock.accept())
-		th.run()
+	Thread(target=AcceptListener).start()
